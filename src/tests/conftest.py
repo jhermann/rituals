fdc5fcaae2b7b79ab7e38098030e6cd9fa4c9140
jhermann <jh@web.de>
2015-02-17 21:16:42 +0100

    standard py.test dynamic configuration

---
+++ src/tests/conftest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=
+""" py.test dynamic configuration.
+"""
+# Copyright ⓒ  2015 Jürgen Hermann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# The full LICENSE file and source are available at
+#    https://github.com/jhermann/rituals
+
+import logging
+
+import pytest
+
+
+# Globally available fixtures
+@pytest.fixture
+def logger(scope='session'):
+    """Test logger instance as a fixture."""
+    logging.basicConfig(level=logging.DEBUG)
+    return logging.getLogger('tests')
