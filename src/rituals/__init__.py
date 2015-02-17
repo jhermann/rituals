eb13c9051d9903ab41f652dede391398786d88f2
jhermann <jh@web.de>
2015-02-17 20:27:45 +0100

    package skeleton

---
+++ src/rituals/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-whitespace
+""" rituals – Common tasks for 'Invoke' that are needed again and again.
+
+    Copyright ⓒ  2015 Jürgen Hermann
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    The full LICENSE file and source are available at
+       https://github.com/jhermann/rituals
+"""
+__url__             = 'https://github.com/jhermann/rituals'
+__version__         = '0.1.0'
+__license__         = 'GPL v2'
+__author__          = 'Jürgen Hermann'
+__author_email__    = 'jh@web.de'
