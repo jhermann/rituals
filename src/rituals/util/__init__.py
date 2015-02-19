d9433435335a6d21b159895959dbf71042a900c8
jhermann <jh@web.de>
2015-02-19 11:42:02 +0100

    :new: util.search_file_upwards

--- src/rituals/util/__init__.py
+++ src/rituals/util/__init__.py
@@ -18,3 +18,19 @@
 #
 # The full LICENSE file and source are available at
 #    https://github.com/jhermann/rituals
+
+import os
+
+
+def search_file_upwards(name, base=None):
+    """ Search for a file named `name` from cwd or given directory to root.
+        Return None if nothing's found.
+    """
+    base = base or os.getcwd()
+    while base != os.path.dirname(base):
+        if os.path.exists(os.path.join(base, name)):
+            return base
+        base = os.path.dirname(base)
+
+    return None
+
---
+++ src/tests/test_util.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=wildcard-import, missing-docstring, redefined-outer-name, invalid-name, no-self-use, bad-continuation
+""" Tests for `rituals.util`.
+"""
+# Copyright ⓒ  2015 Jürgen Hermann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# The full LICENSE file and source are available at
+#    https://github.com/jhermann/rituals
+
+import os
+import unittest
+
+#import pytest
+
+from rituals import util
+
+
+class SearchFileUpwardsTest(unittest.TestCase):
+
+    def test_not_found_terminates(self):
+        assert util.search_file_upwards('only_crazies_would_name_a_file_like_this') is None
+
+    def test_setup_py(self):
+        home = os.path.dirname(__file__)
+        root = util.search_file_upwards('setup.py', base=home)
+        assert os.path.exists(os.path.join(root, 'tasks.py'))
+
+    def test_same_dir(self):
+        home = os.path.dirname(__file__)
+        root = util.search_file_upwards('conftest.py', base=home)
+        assert root == home
+
+    def test_cwd(self):
+        home = os.path.dirname(__file__)
+        cwd = os.getcwd()
+        try:
+            os.chdir(home)
+            root = util.search_file_upwards('conftest.py')
+            assert root == home
+        finally:
+            os.chdir(cwd)
