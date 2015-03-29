d582706441e2636f76f3be8df39f490b45d24900
jhermann <jh@web.de>
2015-03-29 23:33:53 +0200

    :mag: silence bogus pylint error on Py3

--- src/rituals/_compat.py
+++ src/rituals/_compat.py
@@ -31,5 +31,6 @@ def isodate(datestamp=None, microseconds=False):
     """Return current or given time formatted according to ISO-8601."""
     datestamp = datestamp or datetime.datetime.now()
     if not microseconds:
-        datestamp = datestamp - datetime.timedelta(microseconds=datestamp.microsecond)
-    return datestamp.isoformat(b' ' if PY2 else u' ')
+        usecs = datetime.timedelta(microseconds=datestamp.microsecond)
+        datestamp = datestamp - usecs
+    return datestamp.isoformat(b' ' if PY2 else u' ')  # pylint: disable=no-member
