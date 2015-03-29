fea479abc508c45cb96976d1a356ab85eb9a0e63
jhermann <jh@web.de>
2015-03-29 12:13:38 +0200

    :bug: added missing import

--- src/rituals/acts/testing.py
+++ src/rituals/acts/testing.py
@@ -21,6 +21,8 @@
 #    https://github.com/jhermann/rituals
 from __future__ import absolute_import, unicode_literals, print_function
 
+import os
+
 from invoke import run, task
 
 __all__ = ['tox']
