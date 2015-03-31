d7b790182fc7a1ff1e18e1b6d5b903a15427d7f7
jhermann <jh@web.de>
2015-03-31 10:44:52 +0200

    use a namespace for github tasks

--- src/rituals/acts/github.py
+++ src/rituals/acts/github.py
@@ -21,16 +21,23 @@
 #    https://github.com/jhermann/rituals
 from __future__ import absolute_import, unicode_literals, print_function
 
+import sys
+
 from invoke import task
+
+from invoke import Collection, ctask as task
 
 from rituals import config
 from rituals.util import notify
 
 
-@task(name='gh-sync-readme')
-def gh_sync_readme():
+@task(name='sync-readme')
+def sync_readme():
     """Update GH pages from project's README."""
     _ = config.load()
     notify.banner("Syncing GH pages with 'README.md'...")
 
     notify.failure("Not implemented yet!")
+
+
+namespace = Collection.from_module(sys.modules[__name__])  # pylint: disable=invalid-name
