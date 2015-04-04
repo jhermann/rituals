b7ad7cce33ca66382a39977bda888b8ab470eb91
jhermann <jh@web.de>
2015-04-04 14:46:38 +0200

    :bug: exclude .tox/ from bytecode cleaning

--- src/rituals/acts/basic.py
+++ src/rituals/acts/basic.py
@@ -54,6 +54,7 @@ def clean(docs=False, backups=False, bytecode=False, dist=False, # pylint: disab
     # Add patterns based on given parameters
     venv_dirs = ['bin', 'include', 'lib', 'share', 'local', '.venv']
     patterns = ['build/', 'pip-selfcheck.json']
+    excludes = []
     if docs or all:
         patterns.append('docs/_build/')
     if dist or all:
@@ -65,13 +66,14 @@ def clean(docs=False, backups=False, bytecode=False, dist=False, # pylint: disab
             '**/*.py[co]', '**/__pycache__/', '*.egg-info/',
             cfg.srcjoin('*.egg-info/')[len(cfg.project_root)+1:],
         ])
+        excludes.append('.tox/')
     if venv:
         patterns.extend([i + '/' for i in venv_dirs])
     if extra:
         patterns.extend(shlex.split(extra))
 
     # Build fileset
-    patterns = [antglob.includes(i) for i in patterns]
+    patterns = [antglob.includes(i) for i in patterns] + [antglob.excludes(i) for i in excludes]
     if not venv:
         # Do not scan venv dirs when not cleaning them
         patterns.extend([antglob.excludes(i + '/') for i in venv_dirs])
