fa7212213f511185435d10196096543096f0190e
jhermann <jh@web.de>
2015-04-18 08:55:01 +0200

    :arrow_upper_right: suppress the pip version check warning

--- src/rituals/acts/basic.py
+++ src/rituals/acts/basic.py
@@ -122,8 +122,8 @@ def build(ctx, docs=False):
 ))
 def freeze(ctx, local=False):
     """Freeze currently installed requirements."""
-    cmd = 'pip freeze{}'.format(' --local' if local else '')
-    frozen = ctx.run(cmd, hide='out').stdout
+    cmd = 'pip --disable-pip-version-check freeze{}'.format(' --local' if local else '')
+    frozen = ctx.run(cmd, hide='out').stdout.replace('\x1b', '#')
     with io.open('frozen-requirements.txt', 'w', encoding='ascii') as out:
         out.write("# Requirements frozen by 'pip freeze' on {}\n".format(isodate()))
         out.write(frozen)
