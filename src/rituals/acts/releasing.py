8a06677637a72b5ff213abfde5a7e357122ca385
jhermann <jh@web.de>
2015-04-28 16:39:59 +0200

    :arrow_upper_right: added release.pex task (not yet complete)

--- src/rituals/acts/releasing.py
+++ src/rituals/acts/releasing.py
@@ -25,13 +25,55 @@ import io
 import os
 import re
 import sys
+import zipfile
+from contextlib import closing
 
 from invoke import Collection, ctask as task
+from bunch import Bunch
 
 from .. import config
 from ..util import notify, shell
 from ..util.scm import provider as scm_provider
 from ..util.shell import capture
+
+PKG_INFO_MULTIKEYS = ('Classifier',)
+
+
+def get_egg_info(cfg, verbose=False):
+    """Call 'setup egg_info' and return the parsed meta-data."""
+    result = Bunch()
+    setup_py = cfg.rootjoin('setup.py')
+    if not os.path.exists(setup_py):
+        return result
+
+    egg_info = shell.capture("python {} egg_info".format(setup_py), echo=True if verbose else None)
+    for line in egg_info.splitlines():
+        if line.endswith('PKG-INFO'):
+            pkg_info_file = line.split(None, 1)[1]
+            result['__file__'] = pkg_info_file
+            with io.open(pkg_info_file, encoding='utf-8') as handle:
+                lastkey = None
+                for line in handle:
+                    if line.lstrip() != line:
+                        assert lastkey, "Bad continuation in PKG-INFO file '{}': {}".format(pkg_info_file, line)
+                        result[lastkey] += '\n' + line
+                    else:
+                        lastkey, value = line.split(':', 1)
+                        lastkey = lastkey.strip().lower().replace('-', '_')
+                        value = value.strip()
+                        if lastkey in result:
+                            try:
+                                result[lastkey].append(value)
+                            except AttributeError:
+                                result[lastkey] = [result[lastkey], value]
+                        else:
+                            result[lastkey] = value
+
+    for multikey in PKG_INFO_MULTIKEYS:
+        if not isinstance(result.get(multikey, []), list):
+            result[multikey] = [result[multikey]]
+
+    return result
 
 
 @task(help=dict(
@@ -114,6 +156,65 @@ def dist(ctx, devpi=False, egg=False, wheel=False, auto=True):
     ctx.run(' '.join(cmd))
     if devpi:
         ctx.run("devpi upload dist/*")
+
+
+@task(help=dict(
+    upload="Upload the created archive to a WebDAV repository",
+    opts="Extra flags for PEX",
+))
+def pex(ctx, upload=False, opts=''):
+    """Package the project with PEX."""
+    cfg = config.load()
+
+    # Build and check release
+    ctx.run(": invoke clean --all build test check")
+
+    # Get full version
+    pkg_info = get_egg_info(cfg)
+    # from pprint import pprint; pprint(dict(pkg_info))
+    version = pkg_info.version if pkg_info else cfg.project.version
+
+    # Build a PEX for each console entry-point
+    pex_files = []
+    # from pprint import pprint; pprint(cfg.project.entry_points)
+    for script in cfg.project.entry_points['console_scripts']:
+        script, entry_point = script.split('=', 1)
+        script, entry_point = script.strip(), entry_point.strip()
+        pex_file = cfg.rootjoin('bin', '{}-{}.pex'.format(script, version))
+        cmd = ['pex', '-r', cfg.rootjoin('requirements.txt'), cfg.project_root, '-c', script, '-o', pex_file]
+        if opts:
+            cmd.append(opts)
+        ctx.run(' '.join(cmd))
+
+        # Warn about non-portable stuff
+        non_universal = set()
+        with closing(zipfile.ZipFile(pex_file, mode="r")) as pex:
+            for pex_name in pex.namelist():
+                if pex_name.endswith('WHEEL') and '-py2.py3-none-any.whl' not in pex_name:
+                    non_universal.add(pex_name.split('.whl')[0].split('/')[-1])
+        if non_universal:
+            notify.warning("Non-universal or native wheels in PEX '{}':\n    {}"
+                           .format(pex_file.replace(os.getcwd(), '.'), '\n    '.join(sorted(non_universal))))
+            envs = [i.split('-')[-3:] for i in non_universal]
+            envs = {i[0]: i[1:] for i in envs}
+            if len(envs) > 1:
+                envs = {k: v for k, v in envs.items() if not k.startswith('py')}
+            env_id = []
+            for k, v in sorted(envs.items()):
+                env_id.append(k)
+                env_id.extend(v)
+            env_id = '-'.join(env_id)
+            new_pex_file = pex_file.replace('.pex', '-{}.pex'.format(env_id))
+            notify.info("Renamed PEX to '{}'".format(os.path.basename(new_pex_file)))
+            os.rename(pex_file, new_pex_file)
+            pex_file = new_pex_file
+
+        pex_files.append(pex_file)
+
+    if not pex_files:
+        notify.warning("No entry points found in project configuration!")
+    elif upload:
+        pass  # TODO: implement upload
 
 
 @task(
