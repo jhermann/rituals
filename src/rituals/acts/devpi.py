32ce9ec550e00eb84c9db52bdae4c4ff29e3b1c8
jhermann <jh@web.de>
2015-05-11 17:31:56 +0200

    :bug: handle strange 'devpi use' output, e.g. warnings

--- src/rituals/acts/devpi.py
+++ src/rituals/acts/devpi.py
@@ -38,9 +38,13 @@ def get_devpi_url(ctx):
     cmd = 'devpi use --urls'
     lines = ctx.run(cmd, hide='out', echo=False).stdout.splitlines()
     for line in lines:
-        line, base_url = line.split(':', 1)
-        if line.split()[-1].strip() == 'simpleindex':
-            return base_url.split('\x1b')[0].strip().rstrip('/')
+        try:
+            line, base_url = line.split(':', 1)
+        except ValueError:
+            notify.warning('Ignoring "{}"!'.format(line))
+        else:
+            if line.split()[-1].strip() == 'simpleindex':
+                return base_url.split('\x1b')[0].strip().rstrip('/')
 
     raise LookupError("Cannot find simpleindex URL in '{}' output:\n    {}".format(
         cmd, '\n    '.join(lines),
