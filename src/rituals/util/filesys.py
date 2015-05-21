0f2380e37b13a1340bcf5ea19028cf52d0962b6c
jhermann <jh@web.de>
2015-05-21 18:48:34 +0200

    :arrow_upper_right: copied 'rudiments.www.url_as_file' into 'rituals.util.filesys'

--- src/rituals/util/filesys.py
+++ src/rituals/util/filesys.py
@@ -22,7 +22,12 @@
 from __future__ import absolute_import, unicode_literals, print_function
 
 import os
+import tempfile
 from contextlib import contextmanager
+
+import requests
+
+from ._compat import urlparse
 
 
 @contextmanager
@@ -37,3 +42,48 @@ def pushd(path):
         yield saved
     finally:
         os.chdir(saved)
+
+
+# Copied from "rudiments.www"
+@contextmanager
+def url_as_file(url, ext=None):
+    """
+        Context manager that GETs a given `url` and provides it as a local file.
+
+        The file is in a closed state upon entering the context,
+        and removed when leaving it, if still there.
+
+        To give the file name a specific extension, use `ext`;
+        the extension can optionally include a separating dot,
+        otherwise it will be added.
+
+        Parameters:
+            url (str): URL to retrieve.
+            ext (str, optional): Extension for the generated filename.
+
+        Yields:
+            str: The path to a temporary file with the content of the URL.
+
+        Raises:
+            requests.RequestException: Base exception of ``requests``, see its
+                docs for more detailed ones.
+
+        Example:
+            >>> import io, re, json
+            >>> with url_as_file('https://api.github.com/meta', ext='json') as meta:
+            ...     meta, json.load(io.open(meta, encoding='ascii'))['hooks']
+            (u'/tmp/www-api.github.com-Ba5OhD.json', [u'192.30.252.0/22'])
+    """
+    if ext:
+        ext = '.' + ext.strip('.')  # normalize extension
+    url_hint = 'www-{}-'.format(urlparse(url).hostname or 'any')
+
+    content = requests.get(url).content
+    with tempfile.NamedTemporaryFile(suffix=ext or '', prefix=url_hint, delete=False) as handle:
+        handle.write(content)
+
+    try:
+        yield handle.name
+    finally:
+        if os.path.exists(handle.name):
+            os.remove(handle.name)
