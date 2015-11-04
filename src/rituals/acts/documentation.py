73bf1a3b3bb0ed97fce67c49e241c64a3d9b6b93
jhermann <jh@web.de>
2015-11-04 13:55:26 +0100

    :new: docs.upload to WebDAV (Artifactory)

--- src/rituals/acts/documentation.py
+++ src/rituals/acts/documentation.py
@@ -29,6 +29,7 @@ import shutil
 import tempfile
 import textwrap
 import webbrowser
+from contextlib import contextmanager
 
 try:
     from configparser import ConfigParser, Error as ConfigError
@@ -42,8 +43,6 @@ from .. import config
 from ..util import notify
 from ..util.filesys import pushd
 from ..util.shell import capture
-
-PYPI_URL = 'https://pypi.python.org/pypi'
 
 
 def get_pypi_auth(configfile='~/.pypirc'):
@@ -212,47 +211,101 @@ def sphinx(ctx, browse=False, clean=False, watchdog=False, kill=False, status=Fa
         webbrowser.open_new_tab(index_url)
 
 
-@task(help={
-    'browse': "Open index page on successful upload",
-    'pypi': "Upload to {}".format(PYPI_URL),
-})
-def upload(ctx, browse=False, pypi=True):
-    """Upload built Sphinx docs."""
-    cfg = config.load()
+class DocsUploader(object):
+    """Helper to perform an upload of pre-built docs."""
 
-    if not pypi:
-        notify.failure("I have no target to upload to!")
-    html_dir = os.path.join(ctx.rituals.docs.sources, ctx.rituals.docs.build)
-    if not os.path.isdir(html_dir):
-        notify.failure("No HTML docs dir found at '{}'!".format(html_dir))
+    def __init__(self, ctx, cfg, target):
+        self.ctx = ctx
+        self.cfg = cfg
+        self.target = target or ctx.rituals.docs.upload.method
+        self.params = getattr(ctx.rituals.docs.upload.targets, self.target, None)
 
-    if pypi:
-        with pushd(html_dir):
+        if self.params is None:
+            notify.failure("Unknown upload target '{}'!".format(self.target))
+        if not self.params.get('url'):
+            notify.failure("You must provide an upload URL for target '{}', e.g. via the environment:\n"
+                           "    export INVOKE_RITUALS_DOCS_UPLOAD_TARGETS_{}_URL='http://.../{{name}}-{{version}}.zip'"
+                           .format(self.target, self.target.upper()))
+
+    @contextmanager
+    def _zipped(self, docs_base):
+        """ Provide a zipped stream of the docs tree."""
+        with pushd(docs_base):
             with tempfile.NamedTemporaryFile(prefix='pythonhosted-', delete=False) as ziphandle:
                 pass
             zip_name = shutil.make_archive(ziphandle.name, 'zip')
 
-        notify.info("Uploading {:.1f} MiB from '{}'..."
-                    .format(os.path.getsize(zip_name) / 1024.0, zip_name))
+        notify.info("Uploading {:.1f} MiB from '{}' to '{}'..."
+                    .format(os.path.getsize(zip_name) / 1024.0, zip_name, self.target))
         with io.open(zip_name, 'rb') as zipread:
             try:
-                reply = requests.post(PYPI_URL, auth=get_pypi_auth(), allow_redirects=False,
-                              files=dict(content=(cfg.project.name + '.zip', zipread, 'application/zip')),
-                              data={':action': 'doc_upload', 'name': cfg.project.name})
-                if reply.status_code in range(200, 300):
-                    notify.info("{status_code} {reason}".format(**vars(reply)))
-                elif reply.status_code == 301:
-                    url = reply.headers['location']
-                    notify.info("Uploaded docs to '{url}'!".format(url=url))
-
-                    # Open in browser?
-                    if browse:
-                        webbrowser.open_new_tab(url)
-                else:
-                    notify.error("{status_code} {reason}".format(**vars(reply)))
-
+                yield zipread
             finally:
-                pass #os.remove(ziphandle.name)
+                os.remove(ziphandle.name)
+                os.remove(ziphandle.name + '.zip')
+
+    def _to_pypi(self, docs_base, release):
+        """Upload to PyPI."""
+        url = None
+        with self._zipped(docs_base) as handle:
+            reply = requests.post(self.params['url'], auth=get_pypi_auth(), allow_redirects=False,
+                                  files=dict(content=(self.cfg.project.name + '.zip', handle, 'application/zip')),
+                                  data={':action': 'doc_upload', 'name': self.cfg.project.name})
+            if reply.status_code in range(200, 300):
+                notify.info("{status_code} {reason}".format(**vars(reply)))
+            elif reply.status_code == 301:
+                url = reply.headers['location']
+            else:
+                notify.error("{status_code} {reason}".format(**vars(reply)))
+        return url
+
+    def _to_webdav(self, docs_base, release):
+        """Upload to WebDAV store."""
+        url = None
+        with self._zipped(docs_base) as handle:
+            reply = requests.put(self.params['url'].format(name=self.cfg.project.name, version=release),
+                                 data=handle.read(), headers={'Accept': 'application/json'})
+            if reply.status_code in range(200, 300):
+                notify.info("{status_code} {reason}".format(**vars(reply)))
+                try:
+                    data = reply.json()
+                except ValueError as exc:
+                    notify.warning("Didn't get a JSON response!")
+                else:
+                    if 'downloadUri' in data:  # Artifactory
+                        url = data['downloadUri'] + '!/index.html'
+            elif reply.status_code == 301:
+                url = reply.headers['location']
+            else:
+                notify.error("{status_code} {reason}".format(**vars(reply)))
+
+        if not url:
+            notify.warning("Couldn't get URL from upload response!")
+        return url
+
+    def upload(self, docs_base, release):
+        """Upload docs in ``docs_base`` to the target of this uploader."""
+        return getattr(self, '_to_' + self.target)(docs_base, release)
+
+
+@task(help={
+    'browse': "Open index page on successful upload",
+    'target': "Upload target name (default: pypi)",
+    'release': "Version for upload path (default: latest)",
+})
+def upload(ctx, browse=False, target=None, release='latest'):
+    """Upload a ZIP of built Sphinx docs (by default to PyPI, else a WebDAV URL)."""
+    cfg = config.load()
+    uploader = DocsUploader(ctx, cfg, target)
+
+    html_dir = os.path.join(ctx.rituals.docs.sources, ctx.rituals.docs.build)
+    if not os.path.isdir(html_dir):
+        notify.failure("No HTML docs dir found at '{}'!".format(html_dir))
+
+    url = uploader.upload(html_dir, release)
+    notify.info("Uploaded docs to '{url}'!".format(url=url or 'N/A'))
+    if url and browse:  # Open in browser?
+        webbrowser.open_new_tab(url)
 
 
 namespace = Collection.from_module(sys.modules[__name__], name='docs', config={'rituals': dict(
@@ -262,6 +315,13 @@ namespace = Collection.from_module(sys.modules[__name__], name='docs', config={'
         watchdog = dict(
             host = '127.0.0.1',
             port = 8840,
+        ),
+        upload = dict(
+            method = 'pypi',
+            targets = dict(
+                pypi = dict(url='https://pypi.python.org/pypi'),
+                webdav = dict(url=None),  # must be set in the environment
+            ),
         ),
     ),
 )})
