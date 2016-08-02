5d0b2bff1c04afd411cff2b38f3dee805649ad25
jhermann <jh@web.de>
2016-08-02 16:46:33 +0200

    jenkins.description: format RST monospace literals

--- src/rituals/acts/jenkins.py
+++ src/rituals/acts/jenkins.py
@@ -22,6 +22,7 @@
 from __future__ import absolute_import, unicode_literals, print_function
 
 import io
+import re
 import sys
 
 from . import Collection, task
@@ -71,12 +72,16 @@ def description(_dummy_ctx, markdown=False):
     description_file = cfg.rootjoin("build/project.{}".format(markup))
     notify.banner("Creating {} file for Jenkins...".format(description_file))
 
+    long_description = cfg.project.long_description
+    long_description = long_description.replace('\n\n', '</p>\n<p>')
+    long_description = re.sub(r'(\W)``([^`]+)``(\W)', r'\1<tt>\2</tt>\3', long_description)
+
     text = DESCRIPTION_TEMPLATES[markup].format(
         keywords=', '.join(cfg.project.keywords),
         classifiers='\n'.join(cfg.project.classifiers),
         classifiers_indented='    ' + '\n    '.join(cfg.project.classifiers),
         packages=', '.join(cfg.project.packages),
-        long_description_html='<p>{}</p>'.format(cfg.project.long_description.replace('\n\n', '</p>\n<p>')),
+        long_description_html='<p>{}</p>'.format(long_description),
         ##data='\n'.join(["%s=%r" % i for i in cfg.project.iteritems()]),
         **cfg)
     with io.open(description_file, 'w', encoding='utf-8') as handle:
